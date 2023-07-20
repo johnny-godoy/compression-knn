709d1fe74089603b94abd67273c1b9630f608b0f
johnny-godoy <johnny.godoy@ing.uchile.cl>
2023-07-20 10:38:39 -0400

    tests: add more compression algorithms

--- tests/test_compression_length.py
+++ tests/test_compression_length.py
@@ -1,5 +1,7 @@
 """Unit tests for the compression_length function."""
+import bz2
 import gzip
+import lzma
 import unittest
 
 import numpy as np
@@ -13,13 +15,25 @@ class TestGzipCompressionLength(unittest.TestCase):
     def assert_array_less_equal(x, y, err_msg="", verbose=True):
         return assert_array_less_equal(x, y, err_msg=err_msg, verbose=verbose)
 
-    def test_empty_texts(self):
+    def test_empty_texts_gzip(self):
         texts = np.array([], dtype=str)
         expected_lengths = np.array([], dtype=int)
         compressed_lengths = compression_length(texts)
         self.assert_array_less_equal(compressed_lengths, expected_lengths)
 
-    def test_single_text(self):
+    def test_empty_texts_bzip2(self):
+        texts = np.array([], dtype=str)
+        expected_lengths = np.array([], dtype=int)
+        compressed_lengths = compression_length(texts, algorithm="bzip2")
+        self.assert_array_less_equal(compressed_lengths, expected_lengths)
+
+    def test_empty_texts_lzma(self):
+        texts = np.array([], dtype=str)
+        expected_lengths = np.array([], dtype=int)
+        compressed_lengths = compression_length(texts, algorithm="lzma")
+        self.assert_array_less_equal(compressed_lengths, expected_lengths)
+
+    def test_single_text_gzip(self):
         texts = np.array(["This is a single text."], dtype=str)
         expected_lengths = np.array(
             [len(gzip.compress(texts[0].encode("utf-8")))], dtype=int
@@ -27,12 +41,44 @@ class TestGzipCompressionLength(unittest.TestCase):
         compressed_lengths = compression_length(texts)
         self.assert_array_less_equal(compressed_lengths, expected_lengths)
 
-    def test_multiple_texts(self):
+    def test_single_text_bzip2(self):
+        texts = np.array(["This is a single text."], dtype=str)
+        expected_lengths = np.array(
+            [len(bz2.compress(texts[0].encode("utf-8")))], dtype=int
+        )
+        compressed_lengths = compression_length(texts, algorithm="bzip2")
+        self.assert_array_less_equal(compressed_lengths, expected_lengths)
+
+    def test_single_text_lzma(self):
+        texts = np.array(["This is a single text."], dtype=str)
+        expected_lengths = np.array(
+            [len(lzma.compress(texts[0].encode("utf-8")))], dtype=int
+        )
+        compressed_lengths = compression_length(texts, algorithm="lzma")
+        self.assert_array_less_equal(compressed_lengths, expected_lengths)
+
+    def test_multiple_texts_gzip(self):
         texts = np.array(["Text 1", "Text 2", "Text 3"], dtype=str)
         expected_lengths = np.array(
             [len(gzip.compress(t.encode("utf-8"))) for t in texts], dtype=int
         )
         compressed_lengths = compression_length(texts)
+        self.assert_array_less_equal(compressed_lengths, expected_lengths)
+
+    def test_multiple_texts_bzip2(self):
+        texts = np.array(["Text 1", "Text 2", "Text 3"], dtype=str)
+        expected_lengths = np.array(
+            [len(bz2.compress(t.encode("utf-8"))) for t in texts], dtype=int
+        )
+        compressed_lengths = compression_length(texts, algorithm="bzip2")
+        self.assert_array_less_equal(compressed_lengths, expected_lengths)
+
+    def test_multiple_texts_lzma(self):
+        texts = np.array(["Text 1", "Text 2", "Text 3"], dtype=str)
+        expected_lengths = np.array(
+            [len(lzma.compress(t.encode("utf-8"))) for t in texts], dtype=int
+        )
+        compressed_lengths = compression_length(texts, algorithm="lzma")
         self.assert_array_less_equal(compressed_lengths, expected_lengths)
 
 
