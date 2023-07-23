1f8a1b4aaf79240eff540af5ff3ddb8984015992
johnny-godoy <johnny.godoy@ing.uchile.cl>
2023-07-22 23:34:56 -0400

    refactor: create BaseCompressionKNN

--- compression_knn/knn.py
+++ compression_knn/knn.py
@@ -10,9 +10,16 @@ import numpy.typing as npt
 import scipy.stats
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import check_scoring
+from sklearn.metrics import get_scorer_names
+from sklearn.model_selection import BaseCrossValidator
+from sklearn.model_selection import check_cv
+from sklearn.preprocessing import LabelEncoder
 from sklearn.utils._param_validation import Integral
 from sklearn.utils._param_validation import Interval
 from sklearn.utils._param_validation import StrOptions
+from sklearn.utils.validation import check_array
 from sklearn.utils.validation import check_random_state
 
 from compression_knn._compression import algorithms
@@ -27,12 +34,98 @@ with contextlib.suppress(ImportError):  # not in Python < 3.11
 valid_algorithms = set(algorithms.keys())
 
 
-class CompressionKNNClassifier(BaseEstimator, ClassifierMixin):
+class BaseCompressionKNN(BaseEstimator, ClassifierMixin):
+    _parameter_constraints = {
+        "compressor": [StrOptions(valid_algorithms)],
+        "random_state": ["random_state"],
+    }
+
+    def __init__(
+        self,
+        *,
+        n_neighbors,
+        compressor: str = "gzip",
+        random_state: int | np.random.RandomState | None = None,
+    ):
+        self.n_neighbors = n_neighbors
+        self.compressor = compressor
+        self.random_state = random_state
+
+    def _check_neighbors(self, n_neighbors, n_samples: int):
+        if n_neighbors > n_samples:
+            raise ValueError(
+                f"n_neighbors ({n_neighbors}) must be less than or equal to"
+                f" the number of samples ({n_samples})."
+            )
+        return n_neighbors
+
+    def _check_mode(self, n_neighbors, n_classes: int) -> callable:
+        if n_neighbors % n_classes == 0:
+            warnings.warn(
+                f"n_neighbors ({n_neighbors}) is divisible by the number of classes"
+                f" ({n_classes}). This means that ties will be broken randomly."
+            )
+            return functools.partial(mode, rng=self._rng)
+        return lambda x: scipy.stats.mode(x, axis=0)[0]
+
+    def _check_params(self):
+        self._validate_params()
+        self._rng = check_random_state(self.random_state)
+
+    def fit(self, X: npt.ArrayLike[str], y: npt.ArrayLike[str]) -> Self:
+        self._check_params()
+        self.X_, self.y_ = self._validate_data(
+            X, y, accept_sparse=False, ensure_2d=False, dtype="str"
+        )
+        self.X_ = self.X_.reshape((-1, 1))
+        self._encoder = LabelEncoder().fit(self.y_)
+        self.y_ = self._encoder.transform(self.y_)
+        self._n_neighbors = self._check_neighbors(self.n_neighbors, len(self.X_))
+        self._mode = self._check_mode(self._n_neighbors, len(self._encoder.classes_))
+        self.train_lengths_ = compression_length(self.X_, self.compressor)
+        return self  # type: ignore
+
+    def _distance_matrix(self, X: npt.ArrayLike[str]) -> np.ndarray:
+        """Return the distance matrix between X and the training data."""
+        to_arr = check_array(
+            X, dtype="str", ensure_2d=False, allow_nd=False, accept_sparse=False
+        )
+        combination_matrix = np.char.add(to_arr, self.X_)
+        combined_lengths = compression_length(combination_matrix, self.compressor)
+        test_lengths = compression_length(to_arr, self.compressor)
+        max_lengths = np.maximum(self.train_lengths_, test_lengths)
+        min_lengths = np.minimum(self.train_lengths_, test_lengths)
+        distances = (combined_lengths - min_lengths) / max_lengths
+        return distances
+
+    def predict(self, X: npt.ArrayLike[str]) -> np.ndarray[str]:
+        """Predict the class labels for the provided data.
+
+        Parameters
+        ----------
+        X : npt.ArrayLike[str]
+            The data to predict.
+
+        Returns
+        -------
+        np.ndarray[str]
+            The predicted class labels.
+
+        """
+        distances = self._distance_matrix(X)
+        indices = np.argpartition(distances, self.n_neighbors - 1, axis=0)[
+            : self.n_neighbors
+        ]
+        most_common_indexes = self._mode(self.y_[indices])
+        return self._encoder.inverse_transform(self.y_[most_common_indexes])
+
+
+class CompressionKNNClassifier(BaseCompressionKNN):
     r"""A KNN test classifier that uses gzip compression.
 
     Parameters
     ----------
-    n_neighbors : int, default=5
+    n_neighbours : int, default=5
         Number of neighbors to use by default for nearest neighbors queries.
     compressor : str, options={"gzip", "bzip2", "lzma"}, default="gzip"
         The compression algorithm to use.
@@ -61,110 +154,19 @@ class CompressionKNNClassifier(BaseEstimator, ClassifierMixin):
 
     """
     _parameter_constraints = {
+        **BaseCompressionKNN._parameter_constraints,
         "n_neighbors": [Interval(Integral, 1, None, closed="left")],
-        "compressor": [StrOptions(valid_algorithms)],
-        "random_state": ["random_state"],
     }
 
-    def __init__(
-        self,
-        *,
-        n_neighbors: int = 5,
-        compressor: str = "gzip",
-        random_state: int | np.random.RandomState | None = None,
-    ):
-        self.n_neighbors = n_neighbors
-        self.random_state = random_state
-        self.compressor = compressor
 
-    def _check_mode(self, n_classes: int):
-        if self.n_neighbors % n_classes == 0:
-            warnings.warn(
-                f"n_neighbors ({self.n_neighbors}) is divisible"
-                f" by n_classes ({n_classes})."
-                " This may cause ties to be broken randomly, and is not advised."
-            )
-            return functools.partial(mode, rng=self._rng)
-        return lambda x: scipy.stats.mode(x, axis=0)[0]
-
-    def _check_params(self, n_samples: int) -> None:
-        """Check the parameters passed to __init__."""
-        self._validate_params()
-        if self.n_neighbors > n_samples:
-            raise ValueError(
-                "Expected n_neighbors <= n_samples,"
-                f" got {self.n_neighbors} > {n_samples}."
-            )
-        self._rng = check_random_state(self.random_state)
-        self.n_neighbors_ = self.n_neighbors
-
-    def fit(self, X: npt.ArrayLike[str], y: npt.ArrayLike[str]) -> Self:
-        """Fit the k-nearest neighbors classifier under the gzip compression metric.
-
-        Parameters
-        ----------
-        X : npt.ArrayLike[str]
-            The training data.
-        y : npt.ArrayLike[str]
-            The training labels.
-
-        Returns
-        -------
-        Self
-            The fitted estimator.
-
-        """
-        self.X_, self.y_ = self._validate_data(
-            X, y, accept_sparse=False, ensure_2d=False, dtype="str"
-        )
-        self.X_ = self.X_.reshape((-1, 1))
-        self._check_params(len(self.X_))
-        n_classes = len(np.unique(self.y_))
-        self._mode = self._check_mode(n_classes)
-        self.train_lengths_ = compression_length(self.X_)
-        return self  # type: ignore
-
-    def _distance_matrix(self, X: npt.ArrayLike[str]) -> np.ndarray:
-        """Return the distance matrix between X and the training data."""
-        to_arr = np.array(X)
-        combination_matrix = np.char.add(to_arr, self.X_)
-        combined_lengths = compression_length(combination_matrix)
-        test_lengths = compression_length(to_arr)
-        max_lengths = np.maximum(self.train_lengths_, test_lengths)
-        min_lengths = np.minimum(self.train_lengths_, test_lengths)
-        distances = (combined_lengths - min_lengths) / max_lengths
-        return distances
-
-    def predict(self, X: npt.ArrayLike[str]) -> np.ndarray[str]:
-        """Predict the class labels for the provided data.
-
-        Parameters
-        ----------
-        X : npt.ArrayLike[str]
-            The data to predict.
-
-        Returns
-        -------
-        np.ndarray[str]
-            The predicted class labels.
-
-        """
-        distances = self._distance_matrix(X)
-        indices = np.argpartition(distances, self.n_neighbors_ - 1, axis=0)[
-            : self.n_neighbors_
-        ]
-        most_common_indexes = self._mode(indices)
-        return self.y_[most_common_indexes]
-
-
-class CompressionKNNClassifierCV(CompressionKNNClassifier):
+class CompressionKNNClassifierCV(BaseCompressionKNN):
     r"""See `CompressionKNNClassifier`.
     Implement fast hyperparameter tuning for n_neighbors,
       by reusing the distance matrix between the training data and the test data.
 
     Parameters
     ----------
-    n_neighbors : list[int], default=[2, 3, 5, 10]
+    n_neighbors : list[int], default=np.array([2, 3, 5, 10])
         Number of neighbors to use by default for nearest neighbors queries.
     compressor: str, options={"gzip", "bzip2", "lzma"}
         The compression function to use.
@@ -180,17 +182,85 @@ class CompressionKNNClassifierCV(CompressionKNNClassifier):
         - int, to specify the number of folds in a StratifiedKFold,
         - CV splitter
         - An iterable yielding (train, test) splits as arrays of indices.
+    scoring: str, callable, or None, default=None
+        A string (see sklearn's model evaluation documentation) or
+        a scorer callable object / function with signature
+        ``scorer(estimator, X, y)``.
     search_strategy: str, options={"partition", "sort"}
         The search strategy to use when finding the nearest neighbors.
         'partition' uses np.argpartition to find the n_neighbors smallest distances.
         It should be faster than 'sort' when the list of n_neighbors is small.
         'sort' uses np.argsort to find the n_neighbors smallest distances.
         It should be faster than 'partition' when the list of n_neighbors is large.
-
     """
     _parameter_constraints: dict = {
-        **CompressionKNNClassifier._parameter_constraints,
+        **BaseCompressionKNN._parameter_constraints,
         "cv": ["cv_options"],
-        "search_strategy": [StrOptions({"partition", "sort"})],
+        "search_strategy": [
+            StrOptions(
+                {
+                    "partition",
+                    "sort",
+                }
+            )
+        ],
+        "scoring": [
+            StrOptions(set(get_scorer_names())),
+            callable,
+            None,
+        ],
     }
-    ...
+
+    def __init__(
+        self,
+        n_neighbors: np.ndarray[int] = np.array([2, 3, 5, 10]),
+        compressor: str = "gzip",
+        random_state: int | np.random.RandomState | None = None,
+        cv: int | BaseCrossValidator | None = None,
+        scoring: str | callable | None = None,
+        search_strategy: str = "partition",
+    ):
+        super().__init__(
+            n_neighbors=n_neighbors,
+            compressor=compressor,
+            random_state=random_state,
+        )
+        self.cv = cv
+        self.scoring = scoring
+        self.search_strategy = search_strategy
+
+    def _check_neighbors(self, n_neighbors, n_samples: int):
+        arr = check_array(n_neighbors, ensure_2d=False, dtype=int)
+        max_neighbors = np.max(arr)  # type: ignore
+        if max_neighbors > n_samples:
+            raise ValueError(
+                f"The largest n_neighbors {max_neighbors} is larger"
+                f" than the smallest training fold size {n_samples}."
+            )
+        return arr
+
+    def _check_mode(self, n_neighbors, n_classes: int) -> callable:
+        for neighbours in n_neighbors:
+            # Throw warnings for n_neighbors % n_classes == 0
+            super()._check_mode(neighbours, n_classes)
+        # TODO: Use the scipy mode function when it's unbiased
+        return functools.partial(mode, rng=self._rng)
+
+    def _check_params(self):
+        super()._check_params()
+        self._scorer = check_scoring(self, self.scoring, allow_none=True)
+        if self._scorer is None:
+            self._scorer = accuracy_score
+
+    def fit(self, X: npt.ArrayLike[str], y: npt.ArrayLike[str]) -> Self:
+        super().fit(X, y)
+        # Now we need to improve the checking
+        self._cv = check_cv(self.cv, y, classifier=True)
+        self._check_params()
+        # Finish checking the neighbors
+        n_samples = np.min([len(fold) for _, fold in self._cv.split(X, y)])
+        self._n_neighbors = self._check_neighbors(self.n_neighbors, n_samples)
+        # TODO: Implement the search strategies
+        raise NotImplementedError(
+            "Training with cross-validation is not implemented yet."
+        )
